ca280b5a9301b8bae6edca756db9ca2b70182633
misha slavin <mishaslavin@gmail.com>
2012-08-29 02:13:54 -0500

    initial commit

--- chompion-bot.rb
+++ chompion-bot.rb
@@ -1,28 +1,32 @@
 require 'cinch'
 require 'uri'
-#bot = Cinch::Bot.new do
-#  configure do |c|
-#    c.server = "irc.freenode.org"
-#    c.channels = ["##subreddit"]
-#  end
-#  
-#  on :message, /https?:\/\/[\S]+/ do |m|
-#    #POST to the website
-#  end
-#end
+require 'net/http'
+require 'json'
+
+
+
 class Chompion
   include Cinch::Plugin
   
   listen_to :channel
   
-  def post(url)
+  def post(url, user)
     #post to the server
+    http = Net::HTTP.new("127.0.0.1", 3000)
+    
+    request = Net::HTTP::Post.new('/links', initheader = {'Content-Type'=>'application/json'})
+    request.set_form_data({"url"=>url, "user"=>user})
+    response=http.request(request)
+       
+    
   end
   
   def listen(m)
     urls = URI.extract(m.message, "http")
     unless urls.empty?
-      m.reply "url: #{urls}"
+      for url in urls
+        post(url, m.user.nick)
+      end
     end
 
   end
